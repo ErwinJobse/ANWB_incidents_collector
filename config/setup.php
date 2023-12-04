ea2daf35aeabccdf1a37d4a73fd8363af9df50b9
Erwin Jobse <26813479+ErwinJobse@users.noreply.github.com>
2023-12-04 10:56:38 +0100

    Update setup.php

--- config/setup.php
+++ config/setup.php
@@ -1,19 +1,9 @@
 <?php
-<<<<<<< Updated upstream
-
 $databaseHost = "";
 $databaseName = "";
 $databaseCharset = "";
 $datebaseUsername = "";
 $datebasePassword = "";
 $anwbApiKey = "";
-=======
-//add information your own before using the application
-$databaseHost = "localhost";
-$databaseName = "ANWBIncidentCollector";
-$databaseCharset = "utf8mb4";
-$datebaseUsername = "root";
-$datebasePassword = "root";
-$anwbApiKey = "QYUEE3fEcFD7SGMJ6E7QBCMzdQGqRkAi";
-$host = "http://localhost:63342/ANWB_incidents_collector";
->>>>>>> Stashed changes
+
+
