d03822e5d632539feb2e9bb0a82f8d6e4c0beba7
Erwin Jobse <erwin@jobse.org>
2023-12-06 22:57:46 +0100

    added error handling database

--- models/DatabaseModel.php
+++ models/DatabaseModel.php
@@ -14,13 +14,24 @@ class DB
 
     function __construct()
     {
-        $this->pdo = new PDO(
-            "mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";charset=" . DB_CHARSET,
-            DB_USER, DB_PASSWORD, [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-            PDO::ATTR_EMULATE_PREPARES, false
-        ]);
+        $this->connect();
+    }
+
+    // Connect to database
+    private function connect()
+    {
+        try {
+            $this->pdo = new PDO(
+                    "mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";charset=" . DB_CHARSET,
+                    DB_USER, DB_PASSWORD, [
+                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                    PDO::ATTR_EMULATE_PREPARES, false]
+
+            );
+        } catch (PDOException $e) {
+            die("Connection failed: " . $e->getMessage());
+        }
     }
 
     // Close connection
