3271a4b25f8104ef44365e6037956a1a46a42932
Erwin Jobse <26813479+ErwinJobse@users.noreply.github.com>
2023-12-16 21:43:42 +0100

    Create setup.php

---
+++ config/setup.php
@@ -0,0 +1,11 @@
+<?php
+
+$databaseHost = "localhost";
+$databaseName = "yourDatebase";
+$databaseCharset = "utf8mb4";
+$datebaseUsername = "root";
+$datebasePassword = "root";
+$anwbApiKey = "yourKey";
+$host = "localhost";
+$timeZone = "CET";
+
